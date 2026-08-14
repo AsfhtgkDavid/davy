aa8cd6e12be0c6e22b85625b6345f2ae12e5ad30
David Lishchyshen <david@daika.dev>
2026-08-14 10:51:52 +0300

    chore: Add warning only ru content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.daika.davy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
     }
 
     buildTypes {
